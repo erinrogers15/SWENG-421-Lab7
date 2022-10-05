8c1b3f7438301a7d106968651dc4f941db67d27d
erinrogers15 <71192811+erinrogers15@users.noreply.github.com>
2022-10-04 20:17:00 -0400

    base code

--- SWENG 421 Lab7/Program.cs
+++ SWENG 421 Lab7/Program.cs
@@ -1,15 +1,102 @@
 namespace SWENG_421_Lab7
 {
+    public interface NovelElementIF
+    {
+
+    }
+    public interface NovelElementIF2 : NovelElementIF
+    {
+
+    }
+    public abstract class CompositeNovelElement : NovelElementIF
+    {
+
+    }
+    public class Novel : NovelElementIF2, AdminitratorIF
+    {
+        public void save()
+        {
+
+        }
+        public NovelElementIF retrieve()
+        {
+            return null;
+        }
+        public void view()
+        {
+
+        }
+        public void edit()
+        {
+
+        }
+        public void delete()
+        {
+
+        }
+
+    }
+    public class Page : CompositeNovelElement
+    {
+
+    }
+    public class Column : CompositeNovelElement
+    {
+
+    }
+    public class Frame : CompositeNovelElement 
+    {
+
+    }
+    public class Image : NovelElementIF
+    {
+
+    }
+    public class LineOfText : CompositeNovelElement 
+    {
+
+    }
+    public class Character : NovelElementIF
+    {
+
+    }
+    public interface AdminitratorIF: EditorIF, WriterIF
+    {
+
+    }
+    public class Administrator
+    {
+       
+    }
+
+    public interface EditorIF
+    {
+        void save();
+        NovelElementIF retrieve();
+        void delete();
+
+    }
+
+    public class Editor
+    {
+
+    }
+    public interface WriterIF
+    {
+        void view();
+        void edit();
+    }
+    public class Writer
+    {
+
+    }
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
+
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
+
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
